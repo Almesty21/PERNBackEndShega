444e46a5448d214a43c66724f8246acfecf2556f
Mihretab <48807537+Almesty21@users.noreply.github.com>
2023-11-12 17:05:44 -0300

    Update server.js

--- server.js
+++ server.js
@@ -2,13 +2,14 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const collegeRoutes = require('./routes/college.routes');
-
+const getAnalyticsData = require('../config') 
 const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors());
 app.use(express.json());
 app.use('/api/colleges', collegeRoutes);
+ap.use('/getAnalyticsData',getAnalyticsData)
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
